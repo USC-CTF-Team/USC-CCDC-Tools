72844469986c951ab98ddb7da2706cc4f5c3fd36
RJCyber1 <86359182+RJCyber1@users.noreply.github.com>
2025-02-01 20:59:31 -0800

    Create elk-agent.sh

---
+++ Linux Hardening/elk-agent.sh
@@ -0,0 +1,4 @@
+wget https://artifacts.elastic.co/downloads/beats/elastic-agent/elastic-agent-8.17.1-linux-x86_64.tar.gz
+tar xzvf elastic-agent-8.17.1-linux-x86_64.tar.gz
+cd elastic-agent-8.17.1-linux-x86_64
+sudo ./elastic-agent install
